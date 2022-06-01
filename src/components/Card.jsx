0600cfa104b83751c9d7e584dcb1cf0e43e3dcba
thefjrm <felipemartinsplayernet@gmail.com>
2022-06-01 09:53:48 -0300

    Create the first component

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <img src="https://robohash.org/test?200x200" alt="A nice Robo." />
+      <div>
+        <h2>Card Title</h2>
+        <p>jane doe</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
